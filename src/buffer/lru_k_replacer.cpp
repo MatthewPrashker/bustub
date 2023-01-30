ef7a463f5ab777337b90c0352c4de863c236a251
MatthewPrashker <prashker.m@northeastern.edu>
2023-01-30 15:23:48 -0500

    begin lru_replacer

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -17,42 +17,48 @@ namespace bustub {
 
 LRUKNode::LRUKNode(size_t k, frame_id_t fid) : k_(k), fid_(fid) {}
 
-void LRUKNode::SetEvictable(bool evictable) { throw NotImplementedException("SetEvictable not implemented"); }
+void LRUKNode::SetEvictable(bool evictable) { this->is_evictable_ = evictable; }
 
-auto LRUKNode::IsEvictable() -> bool {
-  throw NotImplementedException("IsEvictable not implemented");
-  return false;
-}
+auto LRUKNode::IsEvictable() -> bool { return this->is_evictable_; }
 
-auto LRUKNode::GetFid() -> frame_id_t {
-  throw NotImplementedException("GetFid not implemented");
-  return 0;
-}
+auto LRUKNode::GetFid() -> frame_id_t { return this->fid_; }
 
 auto LRUKNode::GetBackwardKDist(size_t current_timestamp) -> size_t {
-  throw NotImplementedException("GetBackwardKDist not implemented");
-  return 0;
+  if (this->HasInfBackwardKDist()) {
+    return SIZE_T_MAX;
+  }
+  return current_timestamp - this->history_.front();
 }
 
-auto LRUKNode::HasInfBackwardKDist() -> bool {
-  throw NotImplementedException("HasInfBackwardKDist not implemented");
-  return false;
-}
+auto LRUKNode::HasInfBackwardKDist() -> bool { return this->history_.size() < this->k_; }
 
 auto LRUKNode::GetEarliestTimestamp() -> size_t {
-  throw NotImplementedException("GetEarliestTimestamp not implemented");
-  return 0;
+  if (this->history_.size() == 0) {
+    return 0;
+  }
+  return this->history_.front();
 }
 
 void LRUKNode::InsertHistoryTimestamp(size_t current_timestamp) {
-  throw NotImplementedException("InsertHistoryTimestamp not implemented");
+  this->history_.push_back(current_timestamp);
+  if (this->history_.size() > this->k_) {
+    this->history_.pop_front();
+  }
 }
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+    for(size_t f_index = 0; f_index < num_frames; f_index++) {
+        this->node_store_.insert(std::pair(f_index, LRUKNode(k, f_index)));
+    }
+}
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+    auto now = std::chrono::system_clock::now();
+    auto timestamp = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+    this->node_store_.find(frame_id)->second.InsertHistoryTimestamp(timestamp);
+}
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
 
