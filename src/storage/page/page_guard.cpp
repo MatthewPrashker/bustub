e5d1e560cd1200b7c1e2f2f7565a7d037f497b27
MatthewPrashker <prashker.m@northeastern.edu>
2023-02-24 10:15:44 -0500

    passing more page guard tests

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -10,39 +10,42 @@ BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
 
   that.bpm_ = nullptr;
   that.page_ = nullptr;
-  that.is_dirty_ = false;
 }
 
 void BasicPageGuard::Drop() {
   if (this->bpm_ == nullptr) {
     return;
   }
+
   this->bpm_->UnpinPage(this->page_->GetPageId(), this->is_dirty_);
+  this->bpm_ = nullptr;
 }
 
 auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & {
-  if (this == &that) {
+  if (this == &that || that.bpm_ == nullptr) {
     return *this;
   }
-  if (this->page_ != that.page_) {
-    this->Drop();
-  }
-  this->bpm_ = that.bpm_;
-  this->page_ = that.page_;
-  this->is_dirty_ = that.is_dirty_;
 
+  if (this->page_ == that.page_) {
+    this->is_dirty_ = that.is_dirty_;
+    return *this;
+  }
+
+  this->Drop();
+  this->bpm_ = that.bpm_;
+  this->page_ = this->bpm_->FetchPage(that.PageId());
+  this->is_dirty_ = that.is_dirty_;
   return *this;
 }
 
 BasicPageGuard::~BasicPageGuard() { this->Drop(); }
 
-ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept { this->guard_ = std::move(that.guard_); }
+ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept { this->guard_ = BasicPageGuard(std::move(that.guard_)); }
 
 auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & {
   if (this == &that) {
     return *this;
   }
-
   this->guard_ = std::move(that.guard_);
   return *this;
 }
@@ -54,7 +57,9 @@ void ReadPageGuard::Drop() {
 
 ReadPageGuard::~ReadPageGuard() { this->Drop(); }
 
-WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept { this->guard_ = std::move(that.guard_); }
+WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept {
+  this->guard_ = BasicPageGuard(std::move(that.guard_));
+}
 
 auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & {
   if (this == &that) {
